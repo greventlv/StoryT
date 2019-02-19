50bb7f70faf164ca017fe36289b55e2d32939007
greventlv <greventlv@hotmail.com>
2019-02-19 12:54:50 -0500

    Uploading both scripts

---
+++ StorySmith.py
@@ -0,0 +1,20 @@
+# Dynamically construction sentences/paragraphs to help stimulate writing
+
+#name = raw_input('What is your name?\n')
+#print 'Hi, %s.' % name
+#friends = ['john', 'pat', 'gary', 'michael']
+#for i, name in enumerate(friends):
+#    print "iteration {iteration} is {name}".format(iteration=i, name=name)
+
+import random
+#print random.randint(0, 3)
+
+name = ['john', 'pat', 'gary', 'michael']
+verb = ['gazes', 'runs', 'lays about', 'introspects']
+noun = ['weather', 'state of the world', 'insects', 'Buddha']
+
+name1 = random.choice(name)
+verb1 = random.choice(verb)
+noun1 = random.choice(noun)
+
+print  name1 + ' ' + verb1 + ' outside and thinks about the ' + noun1 + '.'
---
+++ StoryTime.py
@@ -0,0 +1,48 @@
+# -*- coding: cp1252 -*-
+npersonself = 'Abe Lincoln'
+npersongood1 = 'Julio Diaz'
+npersongood2 = 'John Candy'
+npersonbadlive1 = 'Hades'
+npersonbad1 = 'Sadam Hussein'
+verb1 = 'shat'
+verbing1 = 'throwing'
+verbing2 = 'vomitting'
+verbing3 = 'playing basketball'
+problem1 = 'Which brand for tomatoes should I buy?'
+amounttime1 = 'one hour'
+amounttime2 = 'years'
+amounttime3 = 'millenia'
+nitemgood1 = 'My Doll'
+nitemsbad1 = 'spiked hats'
+ngroup1 = 'Stabbing Westward'
+selfsaying =  'I must say, I have nothing to say'
+
+#
+npersonself = raw_input('What is your name?\n')
+npersongood1 = raw_input('Who is a good friend to you? Example: ' + npersongood1 + '\n')
+npersongood2 = raw_input('Who is another friend? Example: ' + npersongood2 + '\n')
+npersonbad1 = raw_input('Who is an evil person to you? Example: ' + npersonbad1 + '\n')
+npersonbadlive1 = raw_input('Where does this evil person reside? Example: ' + npersonbadlive1 + '\n')
+verb1 = raw_input('Type a past tense verb. Example: ' + verb1 + '\n')
+verbing1 = raw_input('State a present verb. (acting) Example: ' + verbing1 + '\n')
+verbing2 = raw_input('State a different present verb. Example: ' + verbing2 + '\n')
+verbing3 = raw_input('State another different present verb. Example: ' + verbing3 + '\n')
+problem1 = raw_input('Type a question you have asked yourself. Example: ' + problem1 + '\n')
+amounttime1 = raw_input('Give a measure of time. Example:' + amounttime1 + '\n')
+amounttime2 = raw_input('State a different measure of time. Example: ' + amounttime2 + '\n')
+amounttime3 = raw_input('State another different measure of time. Example: ' + amounttime3 + '\n')
+nitemgood1 = raw_input('What is your favorite Item (thing)? Example: ' + nitemgood1 + '\n')
+nitemsbad1 = raw_input('What is an item you very much dislike. Example: ' + nitemsbad1 + '\n')
+ngroup1 = raw_input('What is your favorite group (musical/political) Example: ' + ngroup1 + '\n')
+selfsaying = raw_input('What is your Favorite saying? Example: ' + selfsaying + '\n')
+
+#Story1 = 'Dear Diary, I ' + verb1 + ' out of my house today. I ' + verb1 + 'ed into ' + npersonbad1 + ' accidentally outside of ' + npersonbadlive1 + '. I had to pass it because ' + npersongood2 + 's house is across from it. ' + npersonbad1 + ' looked at me and threw ' + nitemsbad1 + ' almost hitting me, while I was ' + verbing2 + '. My ' + verbing1 + ' of ' + nitemgood1 + ' made ' + npersonbad1 + ' ' + verb1 + ' away. I finally got to ' + npersongood2 + 's house ' + amounttime2 + ' later, to find ' + npersongood1 + ' was there ' + verbing3 + ' all the members of ' + ngroup1 + '. I was soo happy I shouted ' + selfsaying + ' for about ' + amounttime3 + '. I asked ' + npersongood1 + ' ' + problem1 + ', it took about ' + amounttime1 + ' before i got a clear cut answer, It was due to ' + ngroup1 + ' being there. It was ok, I was satisfied with the answer. All in All today was a good day.'
+#print Story1
+print
+print
+#News1 = 'Jan. 31 —  Amid an intensified U.S. diplomatic push, President ' + npersonself + ' was preparing to meet with British Prime Minister ' + npersongood2 + ' on Friday in a key Camp David summit where the two allies  will discuss forcing ' + npersonbad1 + ' to comply with U.N. resolutions or face war. Meantime, top weapons inspector Hans Blix said he and chief U.N. nuclear inspector ' + npersongood1 + ' were willing to consider inviting ' + npersonbad1 + ' for ' + verbing1 + ' if ' + npersonbadlive1 + ' concedes on some sticking points.  ON THURSDAY, the ' + npersonself + ' administration officials cautioned there was no firm deadline for when talks with allies would cease and the president would make a decision on ' + problem1 + ' But the administration’s timetable of ' + amounttime2 + ', not ' + amounttime1 + ' — uttered by administration officials — was given some specificity. State Department spokesman Richard Boucher said the administration envisions a diplomatic window of “' + amounttime3 + '” — which would coincide with the Feb. 14 report due to the U.N. Security Council from weapons inspectors combing ' + npersonbadlive1 + ' for the banned ' + nitemsbad1 + ' and ' + nitemgood1 + ' ' + npersonself + ' says ' + npersonbad1 + ' has. Several senior administration officials, speaking to  ' + ngroup1 + ' on condition of ' + verbing2 + ' with no one else, said that if ' + npersonbad1 + ' has not disarmed and diplomacy has run its course when the report is filed, ' + npersonself + ' is unlikely to condone more ' + verbing3 + '. One ' + npersonself + ' option is to seek a second U.N. resolution, either authorizing military force or simply declaring the ' + npersonbadlive1 + ' leader in violation of last fall’s ' + verb1 + 'ing decision. ' + npersonself + ' has discussed — but has not committed to — the idea with Italian Prime Minister Silvio Berlusconi, another voice of support amid a mostly skeptical Europe who nonetheless wants U.N. backing for war. ' + npersonself + ' has repeatedly stated ' + selfsaying + ' justifying what was recently discussed with the prime minister.'
+#News2 = 'Jan. 32 - ' + npersonself + 's cyber-security chief resigning. ' + npersongood1 + ' confirms plans and warns of future ' + nitemsbad1 + ' attacks. WASHINGTON, Jan. 31 — ' + npersongood1 + ', the top cyber-security adviser to President ' + npersonself + ', is confirming plans to resign from the White House, raising an ominous warning to colleagues about the destructive effects of future attacks on ' + nitemgood1 + '. ‘As long as we have vulnerabilities and as long as America has enemies in ' + npersonbadlive1 + ', we are at risk with severe damage to ' + nitemgood1 + '.’ ' + npersongood1 + ', Said in an e-mail sent over ' + amounttime3 + ' Thursday to ' + npersongood2 + ', cited damage from the weekend’s infection that struck ' + nitemgood1 + ' with hundreds of thousands of ' + nitemsbad1 + ' worldwide, slowing e-mail and Web surfing and even shutting down some banking systems. Calling the attacking ' + nitemsbad1 + ' dumb and easily and cheaply made, asking the general public the mistifying question ' + problem1 + '. The attack took ' + amounttime1 + ' to complete. “More sophisticated attacks against known vulnerabilities in ' + nitemgood1 + ' could be devastating,” wrote ' + npersongood1 + '. The Associated Press, citing people familiar with ' + npersongood1 + 's plans, reported the decision to resign on Jan. 24. Spending ' + amounttime1 + ' in the White House across three administrations, and was president ' + npersonself + 's counterterrorism coordinator at the time of the Sept. 11 terror attacks. ' + npersongood1 + ' Indicated an interest in a job with ' + npersonbad1 + ', after spending ' + amounttime3 + ' inside the government, infuriated President ' + npersonself + ' ' + verbing2 + ' ' + selfsaying + ' to the associated press, daring a confrontation. ' + ngroup1 + ' was there ' + verbing3 + ' and had nothing to comment except We ' + verb1 + ' on bad people.'
+News3 = 'Oct. 6th - A new movie coming out dealing with the Common Orthodox Church said to hit theaters today, will have Christopher Walken as the young disillusioned Cardinal Pharisey. A french preist turned rogue after he found out he was a flaming homosexual. He commented IT IS! going to, BE! a, Good movie. ' + npersongood1 + ' will play the head of ' + ngroup1 + ', an anti-Church-Reform group based on Al-Queda tactics, saying The Church is fine the way it is, thats why were going to blow it up, killing ourselves in the process. ' + npersonself + ' will play an anti-semitic pope who falls in love with Rabbi ' + npersongood2 + ' a 14 year old castrated boy from Jerusalem. ' + npersonbad1 + ', the late Director, and member of the Church Reform Group, was found dead by many newyorkers last week, after a bomb in his car exploded. Most of the body was razed, but sent his head over busy streets where witnesses tried to preach to him. His last comment alive was; This will be a great movie, its soo crazy. Everyones is getting into their parts really well, they are treating it like a religion! If I didn not know anybetter, Id be scared of getting bombed by ' + ngroup1 + '. Only 2 other parts that were intact were found, his rectum which left streak marks all over the streets, and his scrotum (found inside his mouth). He had only one item at the time of the bombing, a ' + nitemsbad1 + ' attached to his perineum. Specialists dont know if the placement of this (or the scrotum) was due to the explosion. ' + npersongood1 + ' commented I hope he gets cured. ' + npersonself + ' said ' + selfsaying + ' which provided solice for all of us. A few muslims were infront of the New York grand theater protesting against the movie, saying Ach ell varschesezule semetism! which translates to We dont know what semitism is!. Police quickly be-headed the irate crowd, and everyone was filled with mirth.'
+
+
+print News3
