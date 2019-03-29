5092e980926210d36376d27d268b81590ddcbfba
greventlv <greventlv@hotmail.com>
2019-03-28 23:34:25 -0400

    minor updates
    separated some string lists & added inside/out "space" variable

--- StorySmith.py
+++ StorySmith.py
@@ -19,17 +19,20 @@ speaker = win32com.client.Dispatch("SAPI.SpVoice")
 def randomSentence():
     name = ['John', 'Pat', 'Jessi', 'Michelle', 'Ben', 'Rosa', 'Sean', 'Mari', 'Patricio', 'Brian', 'Annie', 'Alvaro']
     verb = ['gazes', 'runs', 'lays about', 'introspects', 'plays', 'cleans', 'cuts', 'drives', 'eats', 'flies', 'reads', 'stops', 'swims', 'throws', 'trips', 'walks', 'washes', 'writes', 'admires', 'adopts', 'beats', 'buys', 'counts', 'doubts', 'discovers', 'desires', 'meditates']
-    noun = ['weather', 'state of the world', 'insects', 'Buddha', 'milk', 'rice', 'snow', 'rain', 'water', 'food', 'music', 'luggage', 'President', 'orangessnail', 'spring', 'idea', 'mouth', 'sea', 'mountain', 'day', 'mice', 'pail', 'bulb', 'cemetery']
+    noun = ['weather', 'state of the world', 'insects', 'Buddha', 'milk', 'rice', 'snow', 'rain', 'water', 'food', 'music', 'luggage', 'President', 'oranges', 'snail', 'spring', 'idea', 'mouth', 'sea', 'mountain', 'day', 'mice', 'pail', 'bulb', 'cemetery', 'mushroom']
+    space = ['inside', 'outside']
 
     name1 = random.choice(name)
     verb1 = random.choice(verb)
     verb2 = random.choice(verb)
     noun1 = random.choice(noun)
+    space1 = random.choice(space)
 
-    print  (name1 + ' ' + verb1 + ' outside and ' + verb2 + ' the ' + noun1 + '.')
-    speaker.Speak(name1 + ' ' + verb1 + ' outside and ' + verb2 + ' the ' + noun1 + '.')
+    print  (name1 + ' ' + verb1 + ' ' + space1 + ' and ' + verb2 + ' the ' + noun1 + '.')
+    speaker.Speak(name1 + ' ' + verb1 + ' ' + space1 + ' and ' + verb2 + ' the ' + noun1 + '.')
 
 # Gui
 
-label1 = Button(master, text="Say something", command=randomSentence)
-label1.pack()
+btnRS = Button(master, text="Say something", command=randomSentence)
+#btnRS.place(relx=0.5, rely=0.5, anchor=CENTER)
+btnRS.pack()
